9d8fe09fa2eb8fd4805f4605c5e79d097ebae80e
Shir <nangiale.shir@sap.com>
2025-12-03 18:46:27 +0100

    ci: patch baseUrl for PR previews in build workflow

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -2,7 +2,6 @@ import {themes as prismThemes} from 'prism-react-renderer';
 import type {Config} from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
 
-// This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 const config: Config = {
   title: 'My Site',
@@ -13,14 +12,13 @@ const config: Config = {
     v4: true,
   },
 
-  // GitHub Pages URL deines Users
+
   url: 'https://nirooxx.github.io',
-  // Projektpfad
+
   baseUrl: '/pr-preview-lab/',
 
-  // GitHub Repo Config
-  organizationName: 'nirooxx',          // dein GitHub-User
-  projectName: 'pr-preview-lab',       // dieses Repo
+  organizationName: 'nirooxx',
+  projectName: 'pr-preview-lab',
 
   onBrokenLinks: 'throw',
 
